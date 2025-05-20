c582db7eae844a4c3f95874f836851068203b1a0
aprovados3 <civilaprovados3@gmail.com>
2025-05-20 17:23:07 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+const dadosAprovados = [
+  { nome: "João Silva", posicao: "1", cargo: "Agente" },
+  { nome: "Carlos Pereira", posicao: "3", cargo: "Agente" },
+  { nome: "Lucas Lima", posicao: "5", cargo: "Agente" },
+  { nome: "Maria Santos", posicao: "2", cargo: "Escrivão" },
+  { nome: "Ana Souza", posicao: "4", cargo: "Escrivão" }
+];
+
+
+
